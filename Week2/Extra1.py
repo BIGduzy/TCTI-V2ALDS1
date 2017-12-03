79dc3d5d46995d8e78ec1e66a9585df6b7540e2f
BIGduzy <nick-b@live.nl>
2017-12-03 17:09:19 +0100

    Week2 Extra1 quick_sort now edits lst

--- Week2/Extra1.py
+++ Week2/Extra1.py
@@ -6,10 +6,11 @@ def quick_sort(a):
         return [a[0], a[1]] if a[0] < a[1] else [a[1], a[0]]
 
     pivot = a[random.randint(0, len(a) - 1)]
-    a.remove(pivot)  # Remove pivot from the list
+    tmp = a[:]
+    tmp.remove(pivot)  # Remove pivot from the list
     left = []
     right = []
-    for el in a:
+    for el in tmp:
         if el <= pivot:
             left.append(el)
         else:
@@ -24,5 +25,31 @@ def quick_sort(a):
     else:
         return [pivot]
 
+
+def quick_sort2(a):
+    if len(a) > 1:
+        pivot = a[random.randint(0, len(a) - 1)]
+        tmp = a[:]
+        tmp.remove(pivot)
+        left = []
+        right = []
+        for el in tmp:
+            if el <= pivot:
+                left.append(el)
+            else:
+                right.append(el)
+
+        if left:
+            quick_sort2(left)
+        if right:
+            quick_sort2(right)
+
+        a[:len(left)] = left
+        a[len(left)] = pivot
+        a[len(left) + 1:] = right
+
 lst = [2, 3, 1, 66, 1, 4, 8, 2, 3]
+
 print(quick_sort(lst))
+quick_sort2(lst)
+print(lst)
